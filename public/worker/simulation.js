f3264a4c9fd86460f5a6af42a991d747c98d893c
Tobias S <>
2023-03-26 12:35:40 +1300

    add web-worker transpile aciton

--- public/worker/simulation.js
+++ public/worker/simulation.js
@@ -1,50 +1,37 @@
-"use strict";
-
-
 function pay_taxes(brutto_amount, tax_rate) {
-    var taxes = Math.max(0.0, brutto_amount) * tax_rate;
-    var netto_amount = brutto_amount - taxes;
+    const taxes = Math.max(0.0, brutto_amount) * tax_rate;
+    const netto_amount = brutto_amount - taxes;
     return netto_amount;
 }
-
 function calculate_invest_retruns(invest_amount, return_rate) { return Math.max(0.0, invest_amount) * return_rate; }
-
 function forecast_inflation_rate(avg_anual_inflation_rate, num_years) { return Math.pow(1.0 + avg_anual_inflation_rate, num_years); }
-
-var DefaultSimulationOutput = {
-    fi_values: new Map(),
-    fail_values: new Map(),
-    max_hour_rate: 0.0
-};
-
 function clone(input) { return JSON.parse(JSON.stringify(input)); }
-
-var HR_TO_ANUAL = 8 /* business hours */ * 20 /* business days */ * 12 /* business month */; // 1920 business hours per year
-
+const HR_TO_ANUAL = 8 /* business hours */ * 20 /* business days */ * 12 /* business month */; // 1920 business hours per year
 function simulate_founding_period(input) {
-    var sim_data = clone(input);
-    var total_founds = sim_data.initial_founds;
-    var total_invest = sim_data.initial_invest;
-    var financial_independence = sim_data.financial_independent;
-    var failed = undefined;
-    for (var year = sim_data.start_founding_year; year < sim_data.founding_period_years; year++) {
-        var inflation_year = forecast_inflation_rate(sim_data.avg_anual_inflation_rate, year);
+    const sim_data = clone(input);
+    let total_founds = sim_data.initial_founds;
+    let total_invest = sim_data.initial_invest;
+    let financial_independence = sim_data.financial_independent;
+    let failed = undefined;
+    let founds = new Map();
+    for (let year = sim_data.start_founding_year; year < sim_data.founding_period_years; year++) {
+        const inflation_year = forecast_inflation_rate(sim_data.avg_anual_inflation_rate, year);
         // earn active income by work           
         if (financial_independence === undefined) {
-            var income_year = (sim_data.adj_hr_by_inflation ? sim_data.hour_rate * inflation_year : sim_data.hour_rate) * HR_TO_ANUAL;
+            const income_year = (sim_data.adj_hr_by_inflation ? sim_data.hour_rate * inflation_year : sim_data.hour_rate) * HR_TO_ANUAL;
             total_founds += pay_taxes(income_year, sim_data.income_tax);
         }
         // earn passive income by investments
-        var invest_returns_year = calculate_invest_retruns(total_invest, sim_data.avg_anual_invest_return);
+        const invest_returns_year = calculate_invest_retruns(total_invest, sim_data.avg_anual_invest_return);
         total_founds += pay_taxes(invest_returns_year, sim_data.invest_tax);
         // do anual spending on expenses            
-        var expenses_year = sim_data.expected_min_anual_expenses * inflation_year;
+        const expenses_year = sim_data.expected_min_anual_expenses * inflation_year;
         if (expenses_year > total_founds) {
             // since we do not have any more liquid founds, we have to use our investments
             // note: investments have to be liquified and taxed
             // note: we also allow the total_invest value to become negative
-            var diff = expenses_year - total_founds;
-            var taxes = diff * sim_data.invest_tax;
+            const diff = expenses_year - total_founds;
+            const taxes = diff * sim_data.invest_tax;
             total_invest -= (diff + taxes);
             total_founds = 0.0;
         }
@@ -53,7 +40,7 @@ function simulate_founding_period(input) {
         }
         if (sim_data.anual_saving_rate > 0.0 && financial_independence === undefined) {
             // note: we only use the appropriate portion (relating to the risk ratio) of the intented saving rate value and put it into investments
-            var invest_year = sim_data.anual_saving_rate * sim_data.saving_risk_ratio;
+            const invest_year = sim_data.anual_saving_rate * sim_data.saving_risk_ratio;
             if (total_founds >= invest_year) {
                 total_invest += invest_year;
             }
@@ -64,29 +51,31 @@ function simulate_founding_period(input) {
             failed = year;
         }
         if (financial_independence === undefined) {
-            var sim_data_clone = clone(sim_data);
+            const sim_data_clone = clone(sim_data);
             sim_data_clone.start_founding_year = year;
             sim_data_clone.financial_independent = year;
             sim_data_clone.initial_founds = total_founds;
             sim_data_clone.initial_invest = total_invest;
-            var total_remaining_founds = simulate_founding_period(sim_data_clone).total_remaining_founds;
+            const { total_remaining_founds } = simulate_founding_period(sim_data_clone);
             if (total_remaining_founds >= 0.0) {
                 financial_independence = year;
             }
         }
+        founds.set(year, total_founds + total_invest);
     }
     // return the remaining founds and (taxed) investments after the simulated founding period
     // note: this value can be negative, indicating that the specified input values are not sustainable!
     return {
+        founds: founds,
         total_remaining_founds: total_founds + pay_taxes(total_invest, sim_data.invest_tax),
         failed: failed,
         financial_independence: financial_independence
     };
 }
 function forward_fill(array) {
-    var filled = new Array();
-    var last_seen_value = undefined;
-    for (var i = 0; i < array.length; i++) {
+    let filled = new Array();
+    let last_seen_value = undefined;
+    for (let i = 0; i < array.length; i++) {
         if (array[i] !== undefined) {
             last_seen_value = filled[i] = array[i];
         }
@@ -101,29 +90,24 @@ function forward_fill(array) {
 }
 function backward_fill(array) { return forward_fill(array.reverse()).reverse(); }
 function forbackd_fill(array) { return backward_fill(forward_fill(array)); }
-
-function run_simulation(input, initial_hour_rate, hour_rate_increment, onProgress) 
-{
-    var hour_rate = initial_hour_rate;
-    var fi_values = new Array(input.founding_period_years);
-    var fail_values = new Array(input.founding_period_years);
-
-    const max_sim_time  = 10 /* seconds */ * 1000; 
-    const sim_start     = Date.now();
-
-    while (true) 
-    {
-
-        var sim_data = clone(input);
+function run_simulation(input, initial_hour_rate = 0.0, hour_rate_increment = 1.0, onProgress) {
+    let hour_rate = initial_hour_rate;
+    const founds = new Map();
+    const fi_values = new Array(input.founding_period_years);
+    const fail_values = new Array(input.founding_period_years);
+    const max_sim_time = 10 /* seconds */ * 1000;
+    const sim_start = Date.now();
+    let total_founds_max = 0;
+    while (true) {
+        const sim_data = clone(input);
         sim_data.hour_rate = hour_rate;
         sim_data.start_founding_year = 0;
-        var output = simulate_founding_period(sim_data);
-
-        if(onProgress)
-        {
-            onProgress((input.founding_period_years - output.financial_independence) / input.founding_period_years);
+        const output = simulate_founding_period(sim_data);
+        founds.set(hour_rate, output.founds);
+        total_founds_max = Math.max(total_founds_max, ...output.founds.values());
+        if (onProgress) {
+            onProgress((input.founding_period_years - (output.financial_independence || 0)) / input.founding_period_years);
         }
-
         // remember minimal hour rate to reach financial independence in X years
         if (output.financial_independence !== undefined && fi_values[output.financial_independence] === undefined) {
             fi_values[output.financial_independence] = hour_rate;
@@ -133,25 +117,23 @@ function run_simulation(input, initial_hour_rate, hour_rate_increment, onProgres
             fail_values[output.failed] = hour_rate;
         }
         // stop simulation, if we found the hour rate that suggests financial independence in one year!
-        if (output.financial_independence == 0) 
-        {
+        if (output.financial_independence == 0) {
             break;
         }
-
         const sim_dt = Date.now() - sim_start;
         const sim_time_penalty = sim_dt / max_sim_time;
         // dynamically adjust hour-rate increment the longe the simulation is running
         hour_rate_increment += (hour_rate_increment * sim_time_penalty);
-
         // try next higher hour rate
         hour_rate += hour_rate_increment;
     }
     return {
+        founds: founds,
         max_hour_rate: hour_rate,
-        fi_values: new Map(forbackd_fill(fi_values).map(function (hour_rate, year) { return [year, hour_rate]; })),
-        fail_values: new Map(forbackd_fill(fail_values).map(function (hour_rate, year) { return [year, hour_rate]; }))
+        total_founds_max: total_founds_max,
+        fi_values: new Map(forbackd_fill(fi_values).map((hour_rate, year) => { return [year, hour_rate]; })),
+        fail_values: new Map(forbackd_fill(fail_values).map((hour_rate, year) => { return [year, hour_rate]; }))
     };
 }
-
-
-self.onmessage = (event) => { postMessage(run_simulation(event.data, 0.0, 1.0, postMessage)); }
+//@ts-ignore
+self.onmessage = (event) => { postMessage(run_simulation(event.data, 0.0, 1.0, postMessage)); };
