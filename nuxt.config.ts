40d6da5c895df5b6007f3e3053e6ee4c3b4ae21d
Tobias S <>
2023-03-26 12:35:10 +1300

    Set site tile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
+    app: 
+    {
+      head: {
+        title: "Life is Money",
+      },
+    },
     css: [
       'vuetify/lib/styles/main.sass',
       '@mdi/font/css/materialdesignicons.min.css',
