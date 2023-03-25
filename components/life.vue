9e92de4f9ca4a65400000f7bac19e3a4888d2471
Tobias S <>
2023-03-26 12:41:20 +1300

    add foundings chart

--- components/life.vue
+++ components/life.vue
@@ -16,49 +16,135 @@
 
 	<!-- chart & summary -->
 	<v-row v-else :key="updateChart">
-		<v-col :cols="chartColumnWidth">
-			<apexchart :options="chartOptions" :series="series" width="100%" ref="chart" />
+		<v-col :cols="chartColumnWidth" :style="`height: ${chartColumnHeight};`">
+			<!-- budget -->
+			<apexchart :options="foundChartOptions" :series="founds" width="100%" ref="foundChart" />
+			<!-- year-hour-rate -->
+			<apexchart :options="hrateChartOptions" :series="series" width="100%" ref="hrateChart" style="transform: translateY(-100%);" />
 		</v-col>
 		<v-col>
-			<v-tabs v-model="openReportTab" color="primary" fixed-tabs grow>
-				<v-tab value="summary">Summary</v-tab>
-				<v-tab value="info">Chart Info</v-tab>
-			</v-tabs>
-			<v-window v-model="openReportTab">
-        		<v-window-item value="summary">
-					<chartsummary :sim_data="sim_data" />
-				</v-window-item>
-				<v-window-item value="info">
-					<chartinfo />
-				</v-window-item>
-			</v-window>
+			<div class="d-flex justify-center align-center">
+        		<v-card flat max-width="640px">
+					<v-tabs v-model="openReportTab" color="primary" fixed-tabs grow density="compact">
+						<v-tab value="summary">Summary</v-tab>
+						<v-tab value="info">Chart Info</v-tab>
+					</v-tabs>
+					<v-window v-model="openReportTab">
+						<v-window-item value="summary">
+							<chartsummary :sim_data="sim_data" />
+						</v-window-item>
+						<v-window-item value="info">
+							<chartinfo />
+						</v-window-item>
+					</v-window>
+				</v-card>
+			</div>
 		</v-col>
 	</v-row>
 </template>
 <script setup lang="ts">
+
 	import useDefaultStore from "@/stores";
-	import { quantile, ISimulationResults } from "@/src/util";
+
+    import { ISimulationResults } from "@/src/simulation";
+	import { quantile, compactNumber } from "@/src/util";
 	import { useDisplay, useTheme } from "vuetify/lib/framework.mjs";
+
+
+	type TSimData = {
+		[scenario in 'user' | 'best' | 'worst']: ISimulationResults;
+	};
 
 	const store = useDefaultStore();
 	store.$onAction(({ name, after }) => { if(name === "simulate") { after(updateChartData); } });
 
 	const openReportTab = ref(1);
-	const chart = ref();
+	const chartColumnHeight = ref('100%');
+
+	const hrateChart = ref();
+	const foundChart = ref();
 	const updateChart = ref(0);
 	const display = useDisplay();
-	const sim_data = ref({});
+	const sim_data = ref({} as TSimData);
 
 	const chartColumnWidth = computed(() => display.width.value > display.height.value ? 6 : 12);
 
 	const theme = useTheme();
 
-	const chartOptions =
+	let founds: any[] = [];
+	let series: any[] = [];
+	let this_chart_cursor_pos = { x: 0, y: 0 };
+	let last_chart_cursor_pos = { x: 0, y: 0 };
+	
+	let updateFoundsChart: number | undefined = undefined;
+
+	const foundChartOptions = 
 	{
 		chart:
 		{
 			type: 'rangeArea',
-			background: theme.current.value.colors.background,
+			background: 'transparent',
+			animations:
+			{
+				enabled: true,
+				speed: 500
+			},
+			toolbar: { show: false }
+		},
+		theme: { mode: theme.current.value.dark ? 'dark' : 'light' },
+		colors: [
+			`${theme.current.value.colors.secondary}60`,
+			`${theme.current.value.colors.secondary}20`,
+		],
+		dataLabels: { enabled: false },
+		stroke:
+		{
+			curve: 'straight',
+			width: 2
+		},
+		legend: { show: false },
+		grid: { show: false },
+		xaxis:
+		{
+			type: 'numeric',
+			min: 0,
+			max: store.founding_period,
+			labels: 
+			{ 
+				minHeight: 40,
+				maxHeight: 40,
+				formatter: (x_value: number) => { return x_value === 0 ? 'Today' : x_value.toFixed(0); } 
+			},
+		},
+		yaxis: 
+		{
+			min: 0.0,
+			max: 100.0,
+			tooltip: { enabled: false },
+			labels: { show: false, minWidth: 80, maxWidth: 80 }
+		},
+		tooltip:
+		{
+			enabled: false,		
+		},
+		title: 
+		{
+			text: undefined,
+			margin: 0
+		},
+		subtitle: 
+		{
+			text: undefined,
+			margin: 0
+		}
+	};
+
+	const hrateChartOptions =
+	{
+		chart:
+		{
+			type: 'rangeArea',
+			background: 'transparent',
 			animations:
 			{
 				enabled: true,
@@ -86,10 +172,7 @@
 				}
 			}
 		},
-		theme:
-		{
-			mode: theme.current.value.dark ? 'dark' : 'light',
-		},
+		theme: { mode: theme.current.value.dark ? 'dark' : 'light' },
 		colors: [
 			theme.current.value.colors.primary,
 			`${theme.current.value.colors.primary}20`,
@@ -97,27 +180,26 @@
 			`${theme.current.value.colors.error}20`,
 			theme.current.value.colors.secondary
 		],
-		dataLabels:
-		{
-			enabled: false
-		},
+		dataLabels: { enabled: false 	},
 		stroke:
 		{
 			curve: 'straight',
 			width: 2
 		},
-		legend: { show: display.lgAndUp.value },
+		legend: { show: false },
 		xaxis:
 		{
 			type: 'numeric',
 			min: 0,
 			max: store.founding_period,
-			// decimalsInFloat: 0
 			labels: 
 			{
-				formatter: (x_value: number) =>
-				{
-					return x_value === 0 ? 'Today' : x_value.toFixed(0);
+				minHeight: 40,
+				maxHeight: 40,
+				formatter: (x_value: number) => 
+				{ 
+					this_chart_cursor_pos.x = x_value; // !attention: this line is important as we fetch the current y-axis values here, that is used to update the 'foudns' chart
+					return x_value === 0 ? 'Today' : x_value.toFixed(0); 
 				}
 			}
 		},
@@ -127,7 +209,16 @@
 			max: 100.0,
 			crosshairs: { show: true },
 			tooltip: { enabled: true },
-			labels: { formatter: hr_rate => `${hr_rate.toFixed(1)} / hr` }
+			labels: 
+			{ 
+				minWidth: 80,
+				maxWidth: 80,
+				formatter: (y_value: number) => 
+				{ 
+					this_chart_cursor_pos.y = y_value; // !attention: this line is important as we fetch the current y-axis values here, that is used to update the 'foudns' chart
+					return `${compactNumber(y_value)} / hr`; 
+				}
+			}
 		},
 		tooltip:
 		{
@@ -137,41 +228,101 @@
 			fixed: 
 			{
 				enabled: true,
-				position: 'topRight'
+				position: 'topRight',
 			},
 			custom: function({ series, seriesIndex, dataPointIndex, w})
 			{
 				const year = dataPointIndex;
-				switch(seriesIndex)
-				{
-					// FI
-					case 0:
-					case 1:
-					case 2:
-					case 3:
-					{
-						const rate = w.config.series[0].data[dataPointIndex].y;
-						const low_rate = Math.ceil([...series[2]].reverse()[0]) + 1;
-
-						const summary = '<div class="summary_tooltip">'
-							+ `<p>Financial independence can be reached in <strong>${year} years</strong> with an hour rate of <strong style="color: ${theme.current.value.colors.primary};">${rate.toFixed(2)}</strong>.</p>`
-							+ '<div>';
-
-						return summary;
-					}
-				}
+				const rate_user = w.config.series[0].data[dataPointIndex].y;
+				const [rate_lower, rate_upper] = w.config.series[1].data[dataPointIndex].y;
 				
-				return "";
+				
+				function find_closest_hrate(founds: Map<number, Map<number, number>>) : Map<number, number>
+				{
+					const rates 		= [...founds.keys()];
+					let closest_rate 	= rates[0] || 0;
+
+					for(let i = 1; i 	< rates.length; i++) 
+					{
+						const this_rate = rates[i] || 0;
+						const diff_a 	= Math.abs(closest_rate - this_chart_cursor_pos.y);
+						const diff_b 	= Math.abs(this_rate - this_chart_cursor_pos.y);
+
+						closest_rate	= (diff_a < diff_b) ? closest_rate : this_rate;
+					}
+
+					return founds.get(closest_rate)!;
+				}
+
+				clearTimeout(updateFoundsChart);
+				updateFoundsChart = setTimeout(() => 
+				{
+					if(this_chart_cursor_pos.x !== last_chart_cursor_pos.x)
+					{
+						last_chart_cursor_pos.x = this_chart_cursor_pos.x;
+
+						const rate_founds 		= sim_data.value.user.founds.get(rate_user);
+						const rate_founds_upper = sim_data.value.best.founds.get(rate_lower);
+						const rate_founds_lower = sim_data.value.worst.founds.get(rate_upper);
+						
+						if(rate_founds !== undefined && rate_founds_lower !== undefined && rate_founds_upper !== undefined)
+						{
+							foundChart.value.chart.updateOptions(
+							{
+								series: 
+								[
+									{ name: 'Founds', type: 'line', data: [...rate_founds.keys()].map(y => { return { x: y, y: rate_founds.get(y) }; }) },
+									{ 
+										name: 'Founds Confidence', 
+										type: 'rangeArea', 
+										data: [...rate_founds.keys()]
+											.map(y => 
+											{ 
+												return { 
+													x: y, 
+													y: [
+														Math.min(rate_founds_lower.get(y)!, rate_founds_upper.get(y)!, rate_founds.get(y)!),
+														Math.max(rate_founds_lower.get(y)!, rate_founds_upper.get(y)!, rate_founds.get(y)!),
+													]
+												}; 
+											}) 
+									}
+								],
+								xaxis: { min: w.globals.minX, max: w.globals.maxX }
+							});
+						}
+					}
+				}, 200);
+				
+
+				const summary = 
+					'<div style="position: relative; overflow: visible;">'
+						+ '<div class="summary_tooltip">'
+							+ `<p>Financial independence can be reached in <strong>${year} years</strong> with an hour rate of <strong style="color: ${theme.current.value.colors.primary};">${compactNumber(rate_user)}</strong>.</p>`
+						+ '<div>';
+					+ '<div>';
+
+				return summary;
 			},
 			x: { formatter: function(x_value: number) { return `in ${x_value} years`; } }
+		},
+		title: 
+		{
+			text: undefined,
+			margin: 0
+		},
+		subtitle: 
+		{
+			text: undefined,
+			margin: 0
 		}
 	};
 
-	let series: any[] = [];
 
-	function updateChartData(data: { [scenario: string]: ISimulationResults })
+	function updateChartData(data: TSimData)
 	{
 		sim_data.value = data;
+
 		const toXY = (series: Map<number, number>) =>
 		{
 			const d = [];
@@ -197,27 +348,21 @@
 		series.push({ name: 'Insufficent Foudning (FI)', type: 'line', data: toXY(data['user'].fail_values) });
 		// series 4
 		series.push({ name: 'IF Confidence', type: 'rangeArea', data: toXY(data['worst'].fail_values).map(xy => { return { x: xy.x, y: [0.0, xy.y] }; }) });
-		// series 5
-		// series.push({ name: 'Founds', type: 'line', data: toXY(data['user'].fail_values) });
 
+		founds.push({ name: 'Founds', type: 'line', data: [...Array(store.founding_period).keys()].map(y => { return { x: y, y: 0 }; }) });
 
-		chartOptions.yaxis.max = quantile([...data['user'].fi_values.values()], 0.9);
-		chartOptions.xaxis.max = store.founding_period;
-		chartOptions.chart.toolbar.tools.customIcons = [
+		foundChartOptions.yaxis.max = Math.max(data.user.total_founds_max, data.worst.total_founds_max, data.best.total_founds_max);
+		hrateChartOptions.yaxis.max = quantile([...data['user'].fi_values.values()], 0.9);
+		hrateChartOptions.xaxis.max = store.founding_period;
+		hrateChartOptions.chart.toolbar.tools.customIcons = [
 		{
 			icon: '<i class="mdi-fit-to-screen mdi v-icon notranslate v-theme--light v-icon--size-x-small" aria-hidden="true" style="transform: translateY(-2px);" />',
 			index: 1,
 			title: 'Reset zoom',
-			click: () =>
-			{
-				// chartOptions.yaxis.max = data['user'].max_hour_rate;
-				// force redrawing of chart
-				updateChart.value++;
-			}
+			click: () => { updateChart.value++; }
 		}];
 
-		chartOptions.legend.show = display.lgAndUp.value;
-
+		chartColumnHeight.value = `${Math.ceil(hrateChart.value.chart.w.globals.svgHeight)}px`;
 		// force redrawing of chart
 		updateChart.value++;
 	}
@@ -225,7 +370,6 @@
 <style>
 .summary_tooltip {
 
-	position: fixed;
 	width: 265px;
 
 	margin: 0px; 
@@ -235,7 +379,5 @@
 	hyphens: auto; 
 	white-space: initial;
 	text-align: right;
-
-	transform: translateX(-300px) translateY(33px);
 }
 </style>
