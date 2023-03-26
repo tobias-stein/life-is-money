ab765d129de3675467cd9b303aa4ba069607c99b
Tobias S <>
2023-03-26 18:26:37 +1300

    add 'target' static and router base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    app: 
-    {
-      head: {
-        title: "Life is Money",
-      },
-    },
-    css: [
-      'vuetify/lib/styles/main.sass',
-      '@mdi/font/css/materialdesignicons.min.css',
-    ],
-    build: {
-      transpile: ["vuetify"],
-    },
-    modules: [
-      '@pinia/nuxt',
-    ],
-    vuetify: {
-      icons: {
-        iconfont: 'mdiSvg',
-       }
-    }
-})
+
+	target: 'static',
+	ssr: false,
+
+	router: {
+		base: '/life-is-money/'
+	},
+
+	app: 
+	{
+		head: {
+			title: "Life is Money",
+		},
+	},
+	css: [
+		'vuetify/lib/styles/main.sass',
+		'@mdi/font/css/materialdesignicons.min.css',
+	],
+	build: {
+		transpile: ["vuetify"],
+	},
+	modules: [
+		'@pinia/nuxt',
+	],
+	vuetify: {
+		icons: {
+			iconfont: 'mdiSvg',
+		}
+	}
+});
