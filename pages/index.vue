eb07fc3bbb5505a6b83cb66944421f88a83c8fee
Tobias S <>
2023-03-27 12:32:49 +1300

    change step name

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@
             onExit: async () => { },
         },
         {
-            name: "Expenses",
+            name: "Expenditures",
             desc: "",
             comp: resolveComponent('expenses'),
             onEnter: async () => { },
